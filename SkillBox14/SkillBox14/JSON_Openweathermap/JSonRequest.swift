1c261d81f9b6111934c850c339bc32ad2a1c55e0
Iliya <iliyaandaleks@mail.ru>
2020-01-22 17:44:46 +0300

    Add Alomofire

---
+++ SkillBox14/SkillBox14/JSON_Openweathermap/JSonRequest.swift
@@ -0,0 +1,31 @@
+//
+//  JSonRequest.swift
+//  SkillBox14
+//
+//  Created by Илья Лобков on 20.01.2020.
+//  Copyright © 2020 Илья Лобков. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class JSonRequest {
+    func loadAlomaCatigories(callback: @escaping ([Weather])->()) {
+        Alamofire.request("http://api.openweathermap.org/data/2.5/forecast?q=Moscow,ru&appid=f4e8241056c46e3ab2fa1983bf196af9&units=metric", method: .get).responseJSON { response in
+            if let objects = response.result.value {
+                do {
+                    let cityWether = try JSONDecoder().decode(Request.self, from: objects as! Data)
+                    var listAloma: [Weather] = []
+                    cityWether.list.forEach { weather in
+                        if let weather = weather {
+                            listAloma.append(weather)
+                        }
+                    }
+                    callback(listAloma)
+                } catch let error {
+                    print(error)
+                }
+            }
+        }
+    }
+}
